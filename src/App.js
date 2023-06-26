bd5e1dc65f35b736c5dc694fd842c0831b186207
kishan-111 <kishankr7909@gmail.com>
2023-06-26 14:51:41 +0530

    day1 on openweathermap api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect,useState } from 'react';
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(postion){
+      setLat(postion.coords.latitude);
+      setLong(postion.coords.longitude);
+    });
+
+    fetch (`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result =>{
+      setData(result)
+      console.log(data)
+    })
+  }, [lat, long]);
+  
+  function getWeather(){
+    fetch (`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result =>{
+      setData(result)
+      console.log(data)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <div>
+          <h1>CITY NAME: {data.name}</h1>
+          <h1>TEMPERATURE: {data.main.temp}</h1>
+          <h1>SUNRISE: {data.sys.sunrise}</h1>
+          <h1>SUNSET: {data.sys.sunset}</h1>
+          <h1>DESCRIPTION: {data.weather[0].description}</h1>
+        </div>
+      ): (
+        <div>
+          <button onClick={getWeather}>Submit</button>
+        </div>
+      )}
+      </div>
+    </>
   );
 }
 
